5496317ad76b35c20825316c990f6f4aa43c168a
jvalenta99 <32281373+jvalenta99@users.noreply.github.com>
2017-11-24 09:45:10 +0100

    sfg

--- Tag16/GemeinsamProjekt/server/server.js
+++ Tag16/GemeinsamProjekt/server/server.js
@@ -15,8 +15,8 @@ var app = express();
 var fs = require( 'fs' );
 var bp = require( 'body-parser' );
 
-var server = app.listen( 3001, function() {
-  console.log( 'Server läuft auf Port 3001.' );
+var server = app.listen( 20001, function() {
+  console.log( 'Server läuft auf Port 20001.' );
 });
 
 app.use( function(req, res, next) {
@@ -29,17 +29,26 @@ app.use( bp.urlencoded( {extended:true} ) ); // POST Daten geparst
 app.use( express.static( 'inc' ) );
 
 
-app.post( '/getprojects', function( req, res) {
-  console.log( req.body );
+app.post( '/saveProjects', function( req, res) {
+
   //fs.writeFile( 'projekte.json', JSON.stringify( { klicks: req.body}), function() {
-  fs.writeFile( 'projekte.json', JSON.stringify( req.body), function() {
-    console.log( 'klicks.json gespeichert.' );
-    res.end( req.body.anzahlklicks );
+  //fs.writeFile( 'maps.json', JSON.stringify(JSON.parse(req.body)) , function() {
+  fs.writeFile( 'maps.json', JSON.stringify(req.body.pro), function() {
+    console.log( 'maps.json gespeichert.' );
+    res.end( "ok" );
   });
+
+});
+
+app.get( '/readProjects', function( req, res) {
+    fs.readFile( 'maps.json', function(err, data) {
+      //res.end( JSON.stringify(JSON.parse(data)) );
+      res.end( JSON.parse(data) );
+    });
 });
 
 
-/*
+
 app.get( '/', function( req, res) {
   fs.readFile( 'index.html', function(err, data) {
     if ( !err ) {
@@ -48,6 +57,8 @@ app.get( '/', function( req, res) {
     }
   });
 });
+
+/*
 
 app.get( '/getsomething', function( req, res) {
     fs.readFile( 'klicks.json', function(err, data) {
